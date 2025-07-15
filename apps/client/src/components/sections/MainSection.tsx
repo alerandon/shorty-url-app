ba2634cd936a239b175990b8f038cf865f4e4c4a
Alejandro Randon <alejandrorandon@gmail.com>
2025-07-15 17:06:49 -0400

    chore(client): styled short urls table

--- apps/client/src/components/sections/MainSection.tsx
+++ apps/client/src/components/sections/MainSection.tsx
@@ -1,14 +1,14 @@
 import type { FC } from 'react';
 import Hero from '../Hero';
 import LinkInput from '../LinkInput';
-import ShortUrlList from '../ShortUrlList';
+import ShortUrlTable from '../ShortUrlTable';
 
 const MainSection: FC = () => {
   return (
     <main className="flex flex-col justify-center items-center p-4 md:p-8">
       <Hero />
       <LinkInput />
-      <ShortUrlList />
+      <ShortUrlTable />
     </main>
   );
 };
