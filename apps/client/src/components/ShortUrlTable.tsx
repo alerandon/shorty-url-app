a381b9cc56639a09bda0d75d2a3fa73740a9a086
Alejandro Randon <alejandrorandon@gmail.com>
2025-07-17 17:11:42 -0400

    chore(client): adjusted styles from component

--- apps/client/src/components/ShortUrlTable.tsx
+++ apps/client/src/components/ShortUrlTable.tsx
@@ -2,36 +2,40 @@ import React from 'react';
 
 const ShortUrlTable: React.FC = () => {
   return (
-    <table className="w-full max-w-xl md:max-w-5xl mx-auto mt-16 md:mt-24 bg-primary rounded-2xl shadow-lg border-collapse md:text-base text-xs">
-      <thead>
-        <tr>
-          <th className="px-5 py-3 md:px-6 md:py-4 text-left">Original URL</th>
-          <th className="px-5 py-3 md:px-6 md:py-4 text-left">Short URL</th>
-          <th className="px-5 py-3 md:px-6 md:py-4 text-left">Created</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="hover:bg-secondary transition-colors duration-200 rounded-2xl">
-          <td className="px-5 py-3 md:px-6 md:py-4">Example 1</td>
-          <td className="px-5 py-3 md:px-6 md:py-4">Example 1</td>
-          <td className="px-5 py-3 md:px-6 md:py-4">Example 1</td>
-        </tr>
-        <tr className="hover:bg-secondary transition-colors duration-200 rounded-2xl">
-          <td className="px-5 py-3 md:px-6 md:py-4">Example 2</td>
-          <td className="px-5 py-3 md:px-6 md:py-4">Example 2</td>
-          <td className="px-5 py-3 md:px-6 md:py-4">Example 2</td>
-        </tr>
-        <tr className="hover:bg-secondary transition-colors duration-200 rounded-2xl">
-          <td className="rounded-bl-2xl px-5 py-3 md:px-6 md:py-4">
-            Example 3
-          </td>
-          <td className="px-5 py-3 md:px-6 md:py-4">Example 3</td>
-          <td className="rounded-br-2xl px-5 py-3 md:px-6 md:py-4">
-            Example 3
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="bg-primary w-full min-h-[450px] max-w-xl md:max-w-5xl mt-16 md:mt-24 mx-auto rounded-2xl shadow-lg overflow-hidden">
+      <table className="w-full mx-auto border-collapse md:text-base text-xs">
+        <thead>
+          <tr className="text-left border-b border-[#e6e6e6]/40">
+            <th className="px-4 py-3 md:px-6 md:py-4 opacity-[40%] font-medium">
+              Original URL
+            </th>
+            <th className="px-4 py-3 md:px-6 md:py-4 opacity-[40%] font-medium">
+              Short URL
+            </th>
+            <th className="px-4 py-3 md:px-6 md:py-4 opacity-[40%] font-medium">
+              Created
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="hover:bg-secondary transition-colors duration-200 rounded-2xl">
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 1</td>
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 1</td>
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 1</td>
+          </tr>
+          <tr className="hover:bg-secondary transition-colors duration-200 rounded-2xl">
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 2</td>
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 2</td>
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 2</td>
+          </tr>
+          <tr className="hover:bg-secondary transition-colors duration-200 rounded-2xl">
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 3</td>
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 3</td>
+            <td className="px-4 py-3 md:px-6 md:py-4">Example 3</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
