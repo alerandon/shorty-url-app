d2b880e7c21f2960e9d56ce9ecb87de3c25291ee
Alejandro Randon <alejandrorandon@gmail.com>
2025-07-21 19:53:51 -0400

    chore: WIP: env vars across monorepo

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,19 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
-import sequelize from './config/database';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', routes);
 app.get('/', (req, res, next) => {
-  res.send({ message: sequelize.config });
+  res.send({ message: 'Hiii' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running in http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
